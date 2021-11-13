588e4127a7e52d12aa723a243b0b53106470a152
Ante <Boskovicante1@gmail.com>
2021-11-13 02:17:17 +0100

    Drugi potez druge partije

--- DrugaIgra.cs
+++ DrugaIgra.cs
@@ -10,7 +10,7 @@ namespace KrizicKruzicDrugaIgra
             Console.WriteLine("Druga osoba ima 0 pobjeda i 0 poraza");
             Console.WriteLine(" | | ");
             Console.WriteLine(" | | ");
-            Console.WriteLine(" | |o");
+            Console.WriteLine(" |x|o");
         }
     }
 }
