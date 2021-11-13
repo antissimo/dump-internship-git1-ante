3293e643cd0d1c62b8709a21caf584102e7c6336
Ante <Boskovicante1@gmail.com>
2021-11-13 17:34:19 +0100

    Kraj druge igre

--- DrugaIgra.cs
+++ DrugaIgra.cs
@@ -6,8 +6,8 @@ namespace KrizicKruzicDrugaIgra
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Prva osoba ima 0 pobjeda i 0 poraza");
-            Console.WriteLine("Druga osoba ima 0 pobjeda i 0 poraza");
+            Console.WriteLine("Prva osoba ima 1 pobjeda i 1 poraza");
+            Console.WriteLine("Druga osoba ima 1 pobjeda i 1 poraza");
             Console.WriteLine("x| |o");
             Console.WriteLine(" | |o");
             Console.WriteLine(" |x|o");
