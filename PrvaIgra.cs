5a37d1d9d6eb5d2022f4e9f522e2f492d46e2eba
Ante <Boskovicante1@gmail.com>
2021-11-13 17:40:31 +0100

    Kraj svih igri

--- PrvaIgra.cs
+++ PrvaIgra.cs
@@ -6,8 +6,8 @@ namespace KrizicKruzicPrvaIgra
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Prva osoba ima 1 pobjeda i 0 poraza");
-            Console.WriteLine("Druga osoba ima 0 pobjeda i 1 poraza");
+            Console.WriteLine("Prva osoba ima 2 pobjeda i 1 poraza");
+            Console.WriteLine("Druga osoba ima 1 pobjeda i 2 poraza");
             Console.WriteLine("o| |x");
             Console.WriteLine("o|o|x");
             Console.WriteLine(" | |x");
